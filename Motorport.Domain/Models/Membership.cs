40669f3588baa797976de6280cf95e25c33beceb
Manuel Alvarado Estanga <alvarado.manuel@live.com>
2019-11-09 22:59:18 -0500

    [#169529164] Added Domain Entities for Authentication

---
+++ Motorport.Domain/Models/Membership.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace Motorport.Domain.Models
+{
+    public class Membership
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public DateTime StartDate { get; set; }
+
+        [Required]
+        public DateTime ExpirationDate { get; set; }
+
+        public int Role { get; set; }
+
+        public int SubscriptionId { get; set; }
+
+        public Subscription Subscription { get; set; }
+
+        public int UserId { get; set; }
+
+        public User User { get; set; }
+
+        #region "Common Fields"
+        public DateTime CreatedAt { get; set; }
+
+        [MaxLength(50)]
+        public string CreatedBy { get; set; }
+
+        public DateTime ModifiedAt { get; set; }
+
+        [MaxLength(50)]
+        public string ModifiedBy { get; set; }
+
+        public bool Active { get; set; }
+        #endregion
+    }
+}
---
+++ Motorport.Domain/Models/User.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace Motorport.Domain.Models
+{
+    public class User
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required,MaxLength(100)]
+        public string Email { get; set; }
+
+        [Required,MaxLength(100)]
+        public string Password { get; set; }
+
+        public Membership Membership { get; set; }
+
+        #region "Common Fields"
+        public DateTime CreatedAt { get; set; }
+
+        [MaxLength(50)]
+        public string CreatedBy { get; set; }
+
+        public DateTime ModifiedAt { get; set; }
+
+        [MaxLength(50)]
+        public string ModifiedBy { get; set; }
+
+        public bool Active { get; set; }
+        #endregion
+    }
+}
