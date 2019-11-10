e1a207fccd19349214e5387dae8847ef8935d2f8
Manuel Alvarado Estanga <alvarado.manuel@live.com>
2019-11-10 01:46:43 -0500

    [#168799065]Updated database: Organizations, Workshops

---
+++ Motorport.Infrastructure/Migrations/20191110064154_AddedMechAndOrganization.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Motorport.Infrastructure.Migrations
+{
+    public partial class AddedMechAndOrganization : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Organizations",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Ruc = table.Column<string>(maxLength: 20, nullable: false),
+                    BussinessName = table.Column<string>(maxLength: 50, nullable: false),
+                    ThumbnailImage = table.Column<string>(nullable: true),
+                    CreatedAt = table.Column<DateTime>(nullable: false),
+                    CreatedBy = table.Column<string>(maxLength: 50, nullable: true),
+                    ModifiedAt = table.Column<DateTime>(nullable: false),
+                    ModifiedBy = table.Column<string>(maxLength: 50, nullable: true),
+                    Active = table.Column<bool>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Organizations", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "MechanicalWorkshops",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    OrganizationId = table.Column<int>(nullable: false),
+                    Name = table.Column<string>(maxLength: 50, nullable: false),
+                    ImageUrl = table.Column<string>(nullable: true),
+                    AverageRate = table.Column<double>(nullable: false),
+                    Street = table.Column<string>(maxLength: 100, nullable: false),
+                    StreetNumber = table.Column<string>(maxLength: 20, nullable: false),
+                    ZipCode = table.Column<string>(maxLength: 20, nullable: false),
+                    City = table.Column<string>(maxLength: 100, nullable: false),
+                    Department = table.Column<string>(maxLength: 100, nullable: false),
+                    Country = table.Column<string>(maxLength: 100, nullable: false),
+                    Latitude = table.Column<double>(nullable: false),
+                    Longitude = table.Column<double>(nullable: false),
+                    CreatedAt = table.Column<DateTime>(nullable: false),
+                    CreatedBy = table.Column<string>(maxLength: 50, nullable: true),
+                    ModifiedAt = table.Column<DateTime>(nullable: false),
+                    ModifiedBy = table.Column<string>(maxLength: 50, nullable: true),
+                    Active = table.Column<bool>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_MechanicalWorkshops", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_MechanicalWorkshops_Organizations_OrganizationId",
+                        column: x => x.OrganizationId,
+                        principalTable: "Organizations",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_MechanicalWorkshops_OrganizationId",
+                table: "MechanicalWorkshops",
+                column: "OrganizationId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "MechanicalWorkshops");
+
+            migrationBuilder.DropTable(
+                name: "Organizations");
+        }
+    }
+}
