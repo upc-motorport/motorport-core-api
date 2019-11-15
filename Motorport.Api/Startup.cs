692816ec559a45485f13e1a3c8b56da1672f97c3
Manuel Alvarado Estanga <alvarado.manuel@live.com>
2019-11-15 18:28:26 -0500

    Fixed routing - lowercase

--- Motorport.Api/Startup.cs
+++ Motorport.Api/Startup.cs
@@ -36,6 +36,7 @@ namespace Motorport.Api
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddRouting(options => options.LowercaseUrls = true);
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
 
             services.AddDbContext<AzureDbContext>(options =>
